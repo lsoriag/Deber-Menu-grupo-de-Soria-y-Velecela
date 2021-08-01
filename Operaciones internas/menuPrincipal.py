cff25e82eabff5c1c635ee40cb3e9ac32197eff4
lsoriag <85829734+lsoriag@users.noreply.github.com>
2021-07-31 22:03:42 -0500

    Add files via upload

---
+++ Operaciones internas/menuPrincipal.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 31 20:08:18 2021
+
+@author: simer03
+"""
+
+from os import system as pantalla_win
+from calculadoras import CalculadorasMenu
+from operacionesConNumeros import OperacionesConNumeros
+from tratamientoLista import menu as Lista
+from menuOperacionesCadenas import menu as OperacionesConCadenas
+
+class MenuPrincipal:
+    def __init__(self):
+        pass
+    
+    def seleccion(self):
+        opc = 0
+        while opc != 5:
+            pantalla_win('cls')
+            print("**Bienvenido al menu principal**")
+            print(" 1) Calculadora")
+            print(" 2) Operación Numeros")
+            print(" 3) Tratamiento de Listas")
+            print(" 4) Operaciones de Cadenas")
+            print(" 5) Salir")
+            print(" ")
+            while True:
+                try:
+                    opc = int(input("Selecciona una opción: "))
+                    if opc > 0 and opc < 6: break
+                    else: print("Favor ingrese una opción correcta.")
+                except ValueError:
+                    print("Ingrese un valor válido.")
+            if opc == 1: CalculadorasMenu().menu()
+            elif opc == 2: OperacionesConNumeros().menu()
+            elif opc == 3: Lista()
+            elif opc == 4: OperacionesConCadenas()
+        else:
+            print("¡Muchas gracias por preferirnos!")
+            input('Pulsa Enter <-- para salir... ')
+            pantalla_win('cls')
+
+
+if __name__ == '__main__':
+    menu = MenuPrincipal()
+    menu.seleccion()
+    
